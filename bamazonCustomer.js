fc4b29ecd6abf19a0f39abb4a374a5418b3377f0
rlawton512 <rocsi54321@gmail.com>
2017-08-16 20:24:22 -0400

    WIPv1

---
+++ bamazonCustomer.js
@@ -0,0 +1,76 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "test1",
+    database: "bamazon_db"
+})
+
+connection.connect(function(err){
+    console.log("Connected as id: "+ connection.threadId);
+    start();
+})
+
+var start = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        var table = new Table({
+            head: ['position', 'item_id','product_name','department_name','price', 'stock_quantity']
+        , colWidths: [10,10,35,20,15,20]
+        });
+
+        for(var i=0; i<res.length; i++){
+        table.push([res[i].position, res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        };
+        console.log(table.toString());
+
+        customer();
+
+})
+
+var customer = function(){
+        inquirer.prompt([
+        {
+            name: "itemID",
+            type: "input",
+            message: "What is the item ID (5 digits) of the product you would like to buy?"
+        },
+        {
+            name: "units",
+            type: "input",
+            message: "How many units of the item would you like to buy?",
+            validate: function(value){
+                if(isNaN(value)==false){
+                    return true;
+                }else{
+                    return false;
+                    console.log("Please enter a number value.")
+                }
+            }
+        }])
+        .then(function(answer){
+            connection.query("SELECT * FROM products WHERE ?", {item_id: answer.itemID},function(err,res){
+                var totalUnits = res[0].stock_quantity
+                if(answer.units > totalUnits){
+                    console.log ("Insufficient quantity!")
+                    customer();
+                } else {
+                var remainingUnits = (res[0].stock_quantity - answer.units)
+                var totalCost = (res[0].price * answer.units);
+                var totalPrice = totalCost.toFixed(2);
+                console.log("\nYou have chosen the following: " + res[0].product_name + "  " + "Total: " + totalPrice + "\n")
+                }
+                connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity:remainingUnits },{item_id: answer.itemID}], function(err, res) {
+            
+                console.log(res.affectedRows + " product(s) updated!\n");
+                })
+            })
+        
+        })
+    }
+}
+
+
---
+++ seed.sql
@@ -0,0 +1,35 @@
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(51209, "leaf blower", "lawn & garden", 56.48, 500)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(51234, "lawn mower", "lawn & garden", 134.47, 250)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(10359,"Big Magic", "books", 12.00, 1500)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(10372, "You are a Badass", "books", 9.71, 2000)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(70825, "Play-Doh Ice Cream Playset", "toys", 18.54, 900)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(40901, "Immersion Blender", "home & kitchen", 20.00, 50)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(10931, "Adjustable iPad Stand", "electronics", 16.14, 500)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(10322, "A Course In Miracles", "books", 18.79, 12225)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(40921, "Marshmallow Roasting Stocks", "home & kitchen", 14.44, 25)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(10954, "Bluetooth Ultra-Slim Keyboard", "electronics", 17.99, 50)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(70844, "Debugging Rubber Duckies", "toys", 5.95, 25789)
+
+INSERT INTO products(item_id, product_id, department_name, price, stock_quantity)
+VALUES(70815, "Moana Surprise Eggs", "toys", 12.99, 150)
